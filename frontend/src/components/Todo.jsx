2194d143b8bd00b6bb89356cdba5b99073872ea2
SManeesha99 <it20649012@my.sliit.lk>
2025-01-07 17:06:26 +0530

    edit and delete tasks

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,21 +1,60 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Todo = () => {
-
   const [task, setTask] = useState([]);
   const [taskTitle, setTaskTitle] = useState("");
   const [taskDescription, setTaskDescription] = useState("");
+  const [editTaskIndex, setEditTaskIndex] = useState(null);
+  const [editTaskTitle, setEditTaskTitle] = useState("");
+  const [editTaskDescription, setEditTaskDescription] = useState("");
 
   const addTask = () => {
-    if(!taskTitle || !taskDescription){
+    if (!taskTitle || !taskDescription) {
       alert("Task Title or Task Description cannot be empty");
-    }
-    else{
-      setTask([...task, {title: taskTitle, description: taskDescription}]);
+      return;
+    } else {
+      setTask([...task, { title: taskTitle, description: taskDescription }]);
       setTaskTitle("");
       setTaskDescription("");
 
+      const modal = new bootstrap.Modal(
+        document.getElementById("exampleModal")
+      );
+      modal.hide();
     }
+  };
+
+  const editTask = () => {
+    if (!editTaskTitle || !editTaskDescription) {
+      alert("Task Title or Task Description cannot be empty");
+      return;
+    }
+
+    const updatedTask = [...task];
+    updatedTask[editTaskIndex] = {
+      title: editTaskTitle,
+      description: editTaskDescription,
+    };
+
+    setTask(updatedTask);
+    setEditTaskIndex(null);
+    setEditTaskTitle("");
+    setEditTaskDescription("");
+
+    const modal = new bootstrap.Modal(document.getElementById("editTaskModal"));
+    modal.hide();
+  };
+
+  const openUpdatedModal = (index) => {
+    setEditTaskIndex(index);
+    setEditTaskTitle(task[index].title);
+    setEditTaskDescription(task[index].description);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTask = [...task];
+    updatedTask.splice(index, 1);
+    setTask(updatedTask);
   };
 
   return (
@@ -30,7 +69,7 @@ const Todo = () => {
         >
           Add Task
         </button>
-        
+
         {/* add task modal */}
         <div
           className="modal fade"
@@ -70,12 +109,12 @@ const Todo = () => {
                     <label for="task-desc" className="col-form-label">
                       Description:
                     </label>
-                    <textarea 
-                      className="form-control" 
+                    <textarea
+                      className="form-control"
                       id="task-desc"
                       value={taskDescription}
                       onChange={(e) => setTaskDescription(e.target.value)}
-                      ></textarea>
+                    ></textarea>
                   </div>
                 </form>
               </div>
@@ -87,11 +126,79 @@ const Todo = () => {
                 >
                   Close
                 </button>
-                <button 
-                  type="button" 
+                <button
+                  type="button"
                   className="btn btn-primary"
                   onClick={addTask}
-                  >
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* edit task modal */}
+        <div
+          className="modal fade"
+          id="editTaskModal"
+          tabindex="-1"
+          aria-labelledby="editModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="editModalLabel">
+                  Edit Task
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div className="mb-3">
+                    <label for="edit-task-tittle" className="col-form-label">
+                      Task Tittle:
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="edit-task-tittle"
+                      value={editTaskTitle}
+                      onChange={(e) => setEditTaskTitle(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label for="edit-task-desc" className="col-form-label">
+                      Description:
+                    </label>
+                    <textarea
+                      className="form-control"
+                      id="edit-task-desc"
+                      value={editTaskDescription}
+                      onChange={(e) => setEditTaskDescription(e.target.value)}
+                    ></textarea>
+                  </div>
+                </form>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={editTask}
+                >
                   Save
                 </button>
               </div>
@@ -102,16 +209,25 @@ const Todo = () => {
         {/* task list */}
         <ul className="list-group mt-4">
           {task.map((t, index) => (
-            <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+            <li
+              className="list-group-item d-flex justify-content-between align-items-center"
+              key={index}
+            >
               <h5>{t.title}</h5>
               <p>{t.description}</p>
               <button
-              className="btn btn-warning btn-sm me-2">
-              Edit
+                className="btn btn-warning btn-sm me-2"
+                data-bs-toggle="modal"
+                data-bs-target="#editTaskModal"
+                onClick={() => openUpdatedModal(index)}
+              >
+                Edit
               </button>
               <button
-              className="btn btn-danger btn-sm me-2">
-              Edit
+                className="btn btn-danger btn-sm me-2"
+                onClick={() => deleteTask(index)}
+              >
+                Delete
               </button>
             </li>
           ))}
